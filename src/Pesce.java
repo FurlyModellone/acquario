972ffe198d56397ffa490e949e1780012bf7c913
mattia.furlan1 <furlan.mattia@icloud.com>
2023-10-09 10:14:38 +0200

    "feat:costruttore"

--- src/Pesce.java
+++ src/Pesce.java
@@ -1,3 +1,12 @@
 public class Pesce {
+    private String colore;
+    private double grandezza;
 
+    public Pesce(String colore, double grandezza)throws Exception{
+        if(grandezza<=0){
+            throw new Exception("pesce inesistente");
+        }
+        this.colore=colore;
+        this.grandezza=grandezza;
+    }
 }
